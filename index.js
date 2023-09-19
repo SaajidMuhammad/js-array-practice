0533cd55043808e204877ecd2f2f7b2b6141dea0
Ruzaik <https://github.com/mrmruzaik/Ruzaik-array-practice.git>
2023-09-19 15:10:02 +0300

    ans_1-20,24,25

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ console.log("After Fruit Added",fruits);
 
 console.log("Answer 2: Array Manipulation");
 
-const numbers = [10, 20, 30, 40, 50];
+const mNumbers = [10, 20, 30, 40, 50];
 
-const doubledArray = numbers.map(element => element * 2);
+const doubledArray = mNumbers.map(element => element * 2);
 
 
 console.log("DoubleUp Numbers", doubledArray);
@@ -117,8 +117,24 @@ console.log("Total of Number", aProduct);
 // Set the center cell to "X" and the top-right cell to "O".
 // Print the resulting 2D array.
 
-// NOT CLEARD
+      console.log("Answer 6: Multi-dimensional Array");
 
+      const tTboard = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+      ];
+
+      tTboard[1][1] = "X";
+      tTboard[0][2] = "O";
+      // for clarification
+
+      console.log(tTboard);
+
+      // final result 
+      for (const row of tTboard) {
+        console.log(row.join(" | "));
+      }
 //--------------------------------------------------------------------------------------------
 
 
@@ -154,6 +170,17 @@ console.log("Total of Number", aProduct);
 // Filter out the numbers that are divisible by 5.
 // Using the filtered array, create a new array where each element is the square of the corresponding number.
 
+console.log("Answer 8: Array Filtering and Mapping");
+
+const aRnumbers = [3, 8, 12, 15, 20, 25];
+
+const filteredNumbers = aRnumbers.filter(aRnumbers => aRnumbers % 5 === 0,);
+
+console.log(filteredNumbers,"Numbers divisible by 5");
+
+const squaredNumbers = filteredNumbers.map(aRnumbers => aRnumbers * aRnumbers);
+
+console.log(squaredNumbers,"new array with squares of filtered numbers"); 
 
 //--------------------------------------------------------------------------------------------
 
@@ -195,72 +222,287 @@ console.log(joinedString);
 // Use the splice method to remove the elements at index 2 and 3.
 // Insert the numbers 12 and 14 in their place.
 
+    console.log("Answer 11: Array Splicing");
+
+    const spileceNumber = [2, 4, 6, 8, 10];
+
+        /* or
+        spileceNumber.splice(2, 2);
+        spileceNumber.splice(2, 0, 12, 14);
+        */ 
+
+    const deleteElement = spileceNumber.splice(2, 2, 12, 14);
+
+    console.log(spileceNumber); 
+
+//--------------------------------------------------------------------------------------------
+
 // Question 12: Array Reversal
 
 // Create an array of strings: ["apple", "banana", "cherry", "date", "elderberry"].
 // Reverse the order of the elements in the array.
+
+    console.log("Answer 12: Array Reversal");
+
+    console.log("Before Reverse: apple, banana, cherry, date, elderberry");
+
+    const reveseFruitorder = ["apple", "banana", "cherry", "date", "elderberry"];
+
+    const result = reveseFruitorder.reverse(0, 5);
+
+    console.log("the Result", result);
+
+//--------------------------------------------------------------------------------------------
 
 // Question 13: Array Intersection
 
 // Create two arrays: [1, 2, 3, 4, 5] and [3, 4, 5, 6, 7].
 // Find the common elements between the two arrays.
 
+      console.log("Answer 13: Array Intersection");
+
+      const findCommon1 = [1, 2, 3, 4, 5];
+
+      const findCommon2 = [3, 4, 5, 6, 7];
+
+      const commonElements = findCommon1.filter(element => findCommon2.includes(element));
+
+      console.log(commonElements);
+
+//--------------------------------------------------------------------------------------------
+
 // Question 14: Array Frequency Count
 
 // Create an array of numbers: [1, 2, 3, 4, 2, 3, 1, 4, 1, 2, 5].
 // Create an object that counts the frequency of each number in the array.
+
+  console.log("Answer 14: Array Frequency Count");
+
+   const freqCount = [1, 2, 3, 4, 2, 3, 1, 4, 1, 2, 5];
+
+   const frequencyCounter = {};
+
+        for (const number of freqCount) {
+          
+          if (frequencyCounter[number]) {
+            
+            frequencyCounter[number]++;
+          } else {
+            
+            frequencyCounter[number] = 1;
+          }
+        }
+
+  console.log(frequencyCounter);
+
+//--------------------------------------------------------------------------------------------
 
 // Question 15: Array Unique Values
 
 // Create an array with duplicate values: [2, 4, 6, 8, 2, 4, 10, 12].
 // Create a new array containing only the unique values from the original array.
 
+console.log("Answer 15: Array Unique Values");
+
+const dupArrays = [2, 4, 6, 8, 2, 4, 10, 12];
+
+const uniqueValue = [...new Set(dupArrays)];
+
+console.log(uniqueValue);
+
+//--------------------------------------------------------------------------------------------
+
 // Question 16: Array Chunking
 
 // Create an array of numbers: [1, 2, 3, 4, 5, 6, 7, 8].
 // Split the array into chunks of size 3 and store them in a new array.
+
+console.log("Answer 16: Array Chunking");
+
+const cNumbers = [1, 2, 3, 4, 5, 6, 7, 8];
+const chunkSize = 3;
+
+// Initialize a new array to store the chunks
+const chunkedArray = [];
+
+// Loop through the original array
+for (let i = 0; i < cNumbers.length; i += chunkSize) {
+  // Slice the original array to create a chunk
+  const chunk = cNumbers.slice(i, i + chunkSize);
+  
+  // Push the chunk into the chunkedArray
+  chunkedArray.push(chunk);
+}
+
+console.log(chunkedArray);
+
+
+
+
+//--------------------------------------------------------------------------------------------
 
 // Question 17: Array FlatMap
 
 // Create an array of arrays: [[1, 2], [3, 4], [5, 6]].
 // Use the flatMap method to create a new array containing all the individual numbers.
 
+      console.log("Answer 17: Array FlatMap");
+
+      const groupArray = [[1, 2], [3, 4], [5, 6]];
+
+      const flatMapArray = groupArray.flatMap(innerArray => innerArray);
+
+      console.log(flatMapArray);
+
+//--------------------------------------------------------------------------------------------
+
 // Question 18: Array Shuffling
 
 // Create an array of numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
 // Shuffle the elements randomly within the array.
+
+      console.log("Answer 18: Array Shuffling");
+
+      let shufflNmbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+          
+      function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          let ranIndex = Math.floor(Math.random() * (i + 1));
+
+          [array[i], array[ranIndex]] = [array[ranIndex], array[i]];
+        }
+        
+      }
+
+      shuffleArray(shufflNmbers);
+      
+      console.log(shufflNmbers);
+
+//--------------------------------------------------------------------------------------------
 
 // Question 19: Array Palindrome Check
 
 // Create an array of strings: ["race", "car", "level", "deified", "hello"].
 // Check if each string in the array is a palindrome (reads the same forwards and backwards).
 
+      console.log("Answer 19: Array Palindrome Check");
+
+            const strings = ["race", "car", "level", "deified", "hello"];
+
+            function isPalindrome(arrWords) {
+              
+              arrWords = arrWords.replace(/ /g, "").toLowerCase();
+                            
+              const reversedStr = arrWords.split("").reverse().join("");
+              
+              
+              return arrWords === reversedStr;
+            }
+              
+              const palindromes = strings.map(arrWords => isPalindrome(arrWords));
+
+      console.log("is Palindrome word ?", palindromes);
+
+//--------------------------------------------------------------------------------------------
+
 // Question 20: Array Insertion and Deletion
 
 // Create an array of animals: ["dog", "cat", "elephant", "lion"].
 // Insert "tiger" after "cat" and delete "elephant" from the array.
+
+      console.log("Answer 20: Array Insertion and Deletion");
+
+        const wildAnimals = ["dog", "cat", "elephant", "lion"];
+
+        // Find the index of "cat"
+        const indexOfCat = wildAnimals.indexOf("cat");
+
+        // Check if "cat" is found in the array
+        if (indexOfCat !== -1) {
+          // Insert "tiger" after "cat"
+          wildAnimals.splice(indexOfCat + 1, 0, "tiger");
+
+          // Delete "elephant" from the array
+          wildAnimals.splice(wildAnimals.indexOf("elephant"), 1);
+        }
+
+        console.log(wildAnimals); // Output: ["dog", "cat", "tiger", "lion"]
+
+//--------------------------------------------------------------------------------------------
 
 // Question 21: Array Element Frequency
 
 // Create an array of characters: ["a", "b", "c", "a", "b", "a", "d", "c"].
 // Count the frequency of each character and store it in an object.
 
+//--------------------------------------------------------------------------------------------
+
 // Question 22: Array Subtraction
 
 // Create two arrays: [1, 2, 3, 4, 5] and [3, 4, 5, 6, 7].
 // Find the elements in the first array that are not present in the second array.
+
+//--------------------------------------------------------------------------------------------
 
 // Question 23: Array Object Transformation
 
 // Create an array of objects, where each object represents a person with "name" and "age" properties.
 // Convert the array of objects into an array of names using the map function.
 
+//--------------------------------------------------------------------------------------------
+
 // Question 24: Array Chunking with Remainder
 
 // Create an array of numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].
 // Split the array into chunks of size 3, and the last chunk can contain fewer elements if needed.
 
+
+console.log("Answer 24: Array Chunking with Remainder");
+
+/*
+const aNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function splitIntoChunks(array, arraySize) {
+  const testSort = [];
+  for (let i = 0; i < array.length; i += arraySize) {
+    testSort.push(array.slice(i, i + arraySize));
+  }
+  return testSort;
+}
+
+const testSort = splitIntoChunks(aNumbers, 3);
+console.log(testSort);
+*/
+
+
+const aAnumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function chunkArray(array, chunkSize) {
+  const chunkedArray = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunkedArray.push(array.slice(i, i + chunkSize));
+    }
+  return chunkedArray;
+}
+
+const chunkedNumbers = chunkArray(aAnumbers, 3);
+console.log(chunkedNumbers);
+
+
+
+
+//--------------------------------------------------------------------------------------------
+
 // Question 25: Array Flatten
 
 // Create a nested array: [1, [2, 3], [4, [5, 6]], 7, [8, 9]].
 // Flatten the array to a single level using recursion or built-in methods.
+
+
+console.log("Answer 25: Array Flatten");
+
+const findFlat = [1, [2, 3], [4, [5, 6]], 7, [8, 9]];
+
+const flattenedArray = findFlat.flat(Infinity);
+console.log(flattenedArray); 
+
